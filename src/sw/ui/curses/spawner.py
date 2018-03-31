8a67a10077cc826a93852c3cc7460ddb12d2c3f4
Michail Pevnev <mpevnev@gmail.com>
2018-03-31 16:50:28 +0300

    Fix not using curses module

--- src/sw/ui/curses/spawner.py
+++ src/sw/ui/curses/spawner.py
@@ -4,23 +4,24 @@ Provides the CursesSpawner class.
 
 
 import sw.ui as ui
+import sw.ui.curses as curses
 
 
 class CursesSpawner(ui.UISpawner):
     """ A curses-based UI spawner. """
 
     def __init__(self):
-        self.screen = initscr()
+        self.screen = curses.initscr()
         self.screen.keypad(True)
-        cbreak()
-        curs_set(0)
-        noecho()
-        start_color()
+        curses.cbreak()
+        curses.curs_set(0)
+        curses.noecho()
+        curses.start_color()
 
     def finish(self):
-        nocbreak()
-        curs_set(1)
-        endwin()
+        curses.nocbreak()
+        curses.curs_set(1)
+        curses.endwin()
 
     def spawn_main_menu(self, data):
         from sw.ui.curses.main_menu import MainMenu
