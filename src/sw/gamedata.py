f90187454f208b37b68e94b4fec357e43a33e4a1
Michail Pevnev <mpevnev@gmail.com>
2018-03-31 12:46:01 +0300

    Start strings reading from YAML files

--- src/sw/gamedata.py
+++ src/sw/gamedata.py
@@ -3,8 +3,23 @@ Game data module.
 """
 
 
+import sw.const as const
+import sw.const.main_menu as mm
+import sw.misc as misc
+
+
 class GameData():
     """ A container for game data and strings. """
 
     def __init__(self):
-        self.strings = {}
+        self.strings = _read_strings()
+
+
+#--------- helper things ---------#
+
+
+def _read_strings():
+    """ Read strings from the data files. """
+    res = {}
+    res[const.MAIN_MENU] = misc.read("strings", mm.STRINGS_FILE)
+    return res
