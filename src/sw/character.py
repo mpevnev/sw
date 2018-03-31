a8f58983ec3edf655a667db4917c9bbba48f43ca
Michail Pevnev <mpevnev@gmail.com>
2018-03-31 20:13:08 +0300

    Remove unused imports

--- src/sw/character.py
+++ src/sw/character.py
@@ -5,17 +5,13 @@ Provides base Character class that Monster and Player classes inherit from.
 """
 
 
-from collections import deque
-from itertools import chain
-
-
 from sw.skill import HasSkills
 from sw.stat import HasStats
 
 
 class Character(HasSkills, HasStats):
     """
-    A base class in the hierarchy of active game entities.
+    The base class in the hierarchy of active game entities.
     """
 
     def __init__(self, recipe):
