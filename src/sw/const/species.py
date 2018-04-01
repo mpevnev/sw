f875eb0eb9b0b746f4f3c89e522e1328becc51da
Michail Pevnev <mpevnev@gmail.com>
2018-04-01 13:18:11 +0300

    Start species module

---
+++ src/sw/const/species.py
@@ -0,0 +1,9 @@
+"""
+Constants for Species module.
+"""
+
+# For reading from YAML dicts
+NAME = "name"
+SHORTNAME = "short name"
+FORBIDDEN_BACKGROUNDS = "forbidden"
+MODIFIERS = "modifiers"
---
+++ src/sw/species.py
@@ -0,0 +1,21 @@
+"""
+Species module.
+
+Provides Species class.
+"""
+
+import sw.const.species as const
+import sw.modifier as mod
+
+
+class Species():
+    """
+    Representation of a playable species with modifiers it applies to the
+    player character.
+    """
+
+    def __init__(self, data):
+        self.name = data[const.NAME]
+        self.shortname = data[const.SHORTNAME]
+        self.forbidden_backgrounds = data[const.FORBIDDEN_BACKGROUNDS]
+        self.modifiers = [mod.Modifier(d) for d in data[const.MODIFIERS]]
