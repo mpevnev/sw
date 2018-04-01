175b6c8bfc8276c6d7b88a944f52fb25b6f516bf
Michail Pevnev <mpevnev@gmail.com>
2018-04-01 17:03:38 +0300

    Make species selection menu use UI-specific data

---
+++ src/sw/const/ui/curses/species_selection.py
@@ -0,0 +1,19 @@
+"""
+Constants for species selection UI.
+"""
+
+DATA_FILE = "species_selection.yaml"
+
+# Strings
+HEADER = "header"
+SUBSCRIPT = "subscript"
+
+# Geometry
+HEADER_OFFSET = "header offset"
+LIST_VERTICAL_OFFSET = "list offset"
+NUM_TILES = "num tiles"
+SPECIES_PER_TILE = "species per tile"
+SUBSCRIPT_OFFSET = "subscript offset"
+
+# Keybindings
+ABORT_KEY = "abort key"
--- src/sw/ui/curses/species_selection.py
+++ src/sw/ui/curses/species_selection.py
@@ -6,8 +6,7 @@ Curses-based species selection UI.
 import mofloc
 
 
-import sw.const as const
-import sw.const.ui.species_selection as ss
+import sw.const.ui.curses.species_selection as ss
 import sw.event.species_selection as event
 import sw.misc as misc
 import sw.ui as ui
@@ -17,31 +16,34 @@ import sw.ui.curses as curses
 class SpeciesSelection(ui.SpeciesSelection):
     """ The species selection UI. """
 
-    def __init__(self, screen, data):
+    def __init__(self, screen, uidata, data):
         super().__init__()
         self.screen = screen
-        _, w = screen.getmaxyx()
-        self.tiles = [
-            screen.derwin(ss.SPECIES_PER_TILE, w // 3, 0, 0),
-            screen.derwin(ss.SPECIES_PER_TILE, w // 3, 0, w // 3),
-            screen.derwin(ss.SPECIES_PER_TILE, w // 3, 0, 2 * w // 3)]
+        self.uidata = uidata
         self.data = data
+        h, w = screen.getmaxyx()
+        num_tiles = uidata[ss.NUM_TILES]
+        self.tiles = [
+            screen.derwin(h, w // num_tiles, 0, i * w // num_tiles)
+            for i in range(num_tiles)]
 
     def draw(self):
         self.screen.erase()
         tile = 0
         sp_index = 0
+        uidata = self.uidata
+        sp_per_tile = uidata[ss.SPECIES_PER_TILE]
         for letter, species in self.enum_species():
-            if sp_index > (tile + 1) * ss.SPECIES_PER_TILE:
+            if sp_index > (tile + 1) * sp_per_tile:
                 tile += 1
-            y = ss.VERTICAL_OFFSET + (sp_index - tile * ss.SPECIES_PER_TILE)
+            y = uidata[ss.LIST_VERTICAL_OFFSET] + (sp_index - tile * sp_per_tile)
             x = 0
             self.tiles[tile].addstr(y, x, f"{letter} - {species.name}")
         for t in self.tiles:
             t.refresh()
-        strings = self.data.strings[const.SPECIES_SELECTION]
-        curses.print_centered(self.screen, 0, strings[ss.HEADER])
-        self.screen.addstr(ss.SPECIES_PER_TILE + 2, 0, strings[ss.SUBSCRIPT])
+        curses.print_centered(self.screen, uidata[ss.HEADER_OFFSET], uidata[ss.HEADER])
+        self.screen.addstr(sp_per_tile + uidata[ss.SUBSCRIPT_OFFSET], 0,
+                           uidata[ss.SUBSCRIPT])
         self.screen.refresh()
         curses.doupdate()
 
@@ -50,7 +52,7 @@ class SpeciesSelection(ui.SpeciesSelection):
         for letter, species in self.enum_species():
             if letter == ch:
                 return (event.CHOOSE_SPECIES, species)
-        if ch == ss.ABORT_KEY:
+        if ch == self.uidata[ss.ABORT_KEY]:
             return (event.ABORT_SPECIES,)
         raise mofloc.NoEvent
 
