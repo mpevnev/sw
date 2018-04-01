2710f159f63cf59184502eab09cc33fa809e0476
Michail Pevnev <mpevnev@gmail.com>
2018-04-01 16:38:51 +0300

    Make background selection menu use UI-specific data

---
+++ src/sw/const/ui/curses/background_selection.py
@@ -0,0 +1,22 @@
+"""
+Constants for background selection menu.
+"""
+
+
+DATA_FILE = "background_selection.yaml"
+
+
+# Strings
+HEADER = "header"
+SUBSCRIPT = "subscript"
+
+# Geometry
+BACKGROUNDS_PER_TILE = "bg per tile"
+HEADER_OFFSET = "header offset"
+LIST_VERTICAL_OFFSET = "list offset"
+NUM_TILES = "num tiles"
+SUBSCRIPT_OFFSET = "subscript offset"
+
+# Keybindings
+ABORT_KEY = "abort key"
+BACK_KEY = "back key"
--- src/sw/ui/curses/background_selection.py
+++ src/sw/ui/curses/background_selection.py
@@ -6,8 +6,7 @@ Curses-based background selection menu.
 import mofloc
 
 
-import sw.const as const
-import sw.const.ui.background_selection as bg
+import sw.const.ui.curses.background_selection as bs
 import sw.event.background_selection as event
 import sw.misc as misc
 import sw.ui as ui
@@ -17,32 +16,35 @@ import sw.ui.curses as curses
 class BackgroundSelection(ui.BackgroundSelection):
     """ The background selection UI. """
 
-    def __init__(self, screen, data, species):
+    def __init__(self, screen, uidata, data, species):
         super().__init__()
         self.screen = screen
-        h, w = screen.getmaxyx()
-        self.tiles = [
-            screen.derwin(h, w // 3, 0, 0),
-            screen.derwin(h, w // 3, 0, w // 3),
-            screen.derwin(h, w // 3, 0, 2 * w // 3)]
         self.data = data
         self.species = species
+        self.uidata = uidata
+        h, w = screen.getmaxyx()
+        num_tiles = uidata[bs.NUM_TILES]
+        self.tiles = [
+            screen.derwin(h, w // num_tiles, 0, i * w // num_tiles)
+            for i in range(num_tiles)]
 
     def draw(self):
         self.screen.erase()
         tile = 0
         bg_index = 0
+        uidata = self.uidata
+        bg_per_tile = uidata[bs.BACKGROUNDS_PER_TILE]
         for letter, background in self.enum_backgrounds():
-            if bg_index > (tile + 1) * bg.BACKGROUNDS_PER_TILE:
+            if bg_index > (tile + 1) * bg_per_tile:
                 tile += 1
-            y = bg.VERTICAL_OFFSET + (bg_index - tile * bg.BACKGROUNDS_PER_TILE)
+            y = uidata[bs.LIST_VERTICAL_OFFSET] + (bg_index - tile * bg_per_tile)
             x = 0
             self.tiles[tile].addstr(y, x, f"{letter} - {background.name}")
         for t in self.tiles:
             t.refresh()
-        strings = self.data.strings[const.BACKGROUND_SELECTION]
-        curses.print_centered(self.screen, 0, strings[bg.HEADER])
-        self.screen.addstr(bg.BACKGROUNDS_PER_TILE + 2, 0, strings[bg.SUBSCRIPT])
+        curses.print_centered(self.screen, uidata[bs.HEADER_OFFSET], uidata[bs.HEADER])
+        self.screen.addstr(bg_per_tile + uidata[bs.SUBSCRIPT_OFFSET], 0,
+                           uidata[bs.SUBSCRIPT])
         self.screen.refresh()
         curses.doupdate()
 
@@ -51,9 +53,9 @@ class BackgroundSelection(ui.BackgroundSelection):
         for letter, background in self.enum_backgrounds():
             if letter == ch:
                 return (event.CHOOSE_BACKGROUND, background)
-        if ch == bg.ABORT_KEY:
+        if ch == self.uidata[bs.ABORT_KEY]:
             return (event.ABORT_BACKGROUND,)
-        if ch == bg.BACK_KEY:
+        if ch == self.uidata[bs.BACK_KEY]:
             return (event.BACK_TO_SPECIES,)
         raise mofloc.NoEvent
 
