1aed0891b9cd814901130e14e4dfdb3d810f16c2
Michail Pevnev <mpevnev@gmail.com>
2018-04-07 13:29:20 +0300

    Split instantiating a player into two separate classes

--- src/sw/player.py
+++ src/sw/player.py
@@ -17,28 +17,28 @@ class Player(Character):
         self.species = None
         self.background = None
 
-    @staticmethod
-    def from_data(data):
-        """ Create a character from a data dict. """
-        raise NotImplementedError
 
-    def from_scratch(name, species, background):
-        """ Create a wholly new character. """
-        res = Player()
-        res.name = name
-        res.species = species
-        res.background = background
-        res._apply_species()
-        res._apply_background()
-        return res
+class PlayerFromData(Player):
+    """ A player created from a data dict. """
 
-    #--------- species manipulation ---------#
+    def __init__(self):
+        super().__init__()
+
+
+class PlayerFromScratch(Player):
+    """ A player created from the data received from the user. """
+
+    def __init__(self, name, species, background):
+        super().__init__()
+        self.name = name
+        self.species = species
+        self.background = background
+        self._apply_species()
+        self._apply_background()
 
     def _apply_species(self):
         """ Apply species' modifiers. """
         self.add_innate_modifiers(*self.species.modifiers)
-
-    #--------- background manipulation ---------#
 
     def _apply_background(self):
         """ Apply background's modifiers. """
