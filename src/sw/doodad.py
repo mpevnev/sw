020413c840654b71224119a42d9991a3a57b1ca5
Michail Pevnev <mpevnev@gmail.com>
2018-04-14 19:14:52 +0300

    Start doodad module

---
+++ src/sw/doodad.py
@@ -0,0 +1,48 @@
+"""
+Doodad module.
+
+Provides base Doodad class and several subclasses.
+"""
+
+
+from sw.entity import Entity
+
+
+#--------- base class ---------#
+
+
+class Doodad(Entity):
+    """
+    Some passive or reactive game object - a wall, a water cell, whatever.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.detectable = True
+        self.detected = True
+        self.dead = False
+
+    #--------- generic usage by other entities ---------#
+
+    def usable_by_monster(self, monster):
+        """
+        Return True if a given monster can use this object, False otherwise.
+        """
+        raise NotImplementedError
+
+    def usable_by_player(self, player):
+        """
+        Return True if the player can use this object, False otherwise.
+        """
+        raise NotImplementedError
+
+    #--------- death logic ---------#
+
+    def alive(self):
+        return not self.dead
+
+    def death_action(self, state, area, ui):
+        raise NotImplementedError
+
+    def die(self):
+        self.dead = True
