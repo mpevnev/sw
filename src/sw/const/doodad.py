70b4c248fac94166e4ad6b858acf6508b343bde9
Michail Pevnev <mpevnev@gmail.com>
2018-04-15 13:56:19 +0300

    Start populating doodad types

---
+++ src/sw/const/doodad.py
@@ -0,0 +1,17 @@
+"""
+Doodad constants.
+"""
+
+
+from enum import Enum
+
+
+class DoodadType(Enum):
+    """ Doodad types enum. """
+
+    WALL = "wall"
+
+
+# Recipe and save dicts keys
+ID = "id"
+TYPE = "type"
--- src/sw/doodad.py
+++ src/sw/doodad.py
@@ -5,6 +5,8 @@ Provides base Doodad class and several subclasses.
 """
 
 
+import sw.const.doodad as const
+from sw.const.entity import CollisionGroup
 from sw.entity import Entity
 
 
@@ -16,8 +18,10 @@ class Doodad(Entity):
     Some passive or reactive game object - a wall, a water cell, whatever.
     """
 
-    def __init__(self):
+    def __init__(self, subtype, recipe_id):
         super().__init__()
+        self.recipe_id = recipe_id
+        self.subtype = subtype
         self.detectable = True
         self.detected = True
         self.dead = False
@@ -60,3 +64,43 @@ class Doodad(Entity):
 
     def die(self):
         self.dead = True
+
+
+#--------- concrete subclasses ---------#
+
+
+class Wall(Doodad):
+    """ A base class for walls. """
+
+    def __init__(self, recipe_id):
+        super().__init__(const.DoodadType.WALL, recipe_id)
+        self.add_collision_group(CollisionGroup.WALL)
+
+    def use_by_monster(self, monster, state, area, ui):
+        return False
+
+    def use_by_player(self, player, state, area, ui):
+        return False
+
+    def death_action(self, state, area, ui):
+        pass
+
+
+#--------- doodad generation from recipes ---------#
+
+
+def doodad_from_recipe(self, recipe):
+    """ Create a doodad from a recipe (a YAML dict with no instance info). """
+    recipe_id = recipe[const.ID]
+    subtype = recipe[const.TYPE]
+    if subtype == const.DoodadType.WALL.value:
+        return Wall(recipe_id)
+    raise ValueError(f"Unknown doodad type '{subtype}'")
+
+
+#--------- doodad generation from saves ---------#
+
+
+def doodad_from_save(self, save_dict):
+    """ Create a doodad from a saved dict (a YAML dict with instance info). """
+    raise NotImplementedError
