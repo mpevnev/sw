848c243a14252c7f1eaa7408d8499183d70e79a1
Michail Pevnev <mpevnev@gmail.com>
2018-04-21 23:21:15 +0300

    Add a message channel for modifier ticks.

--- src/sw/const/message.py
+++ src/sw/const/message.py
@@ -9,5 +9,6 @@ from enum import Enum, auto
 class Channel(Enum):
     """ A message channel enumeration. """
 
-    NORMAL = auto()
-    MONSTER_DEATH = auto()
+    MODIFIER_TICK = "modtick"
+    MONSTER_DEATH = "mondeath"
+    NORMAL = "normal"
