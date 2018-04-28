dd8ea2f7cc3fd99d0e93c84e907152a3443a17c6
Michail Pevnev <mpevnev@gmail.com>
2018-04-29 00:20:42 +0300

    Update area header module docstrings

--- src/sw/area_header.py
+++ src/sw/area_header.py
@@ -18,6 +18,12 @@ class AreaHeader():
     """
 
     def __init__(self, data):
+        """
+        Create an area header.
+
+        :param data: a game data object needed for a later area generation.
+        :type data: sw.gamedata.GameData
+        """
         self.data = data
         self.name = None
         self.biome = None
@@ -25,7 +31,12 @@ class AreaHeader():
         self.hostility = HostilityLevel.SAFE
 
     def load_or_generate_area(self):
-        """ Either load an area from a file, or generate it from scratch. """
+        """
+        Either load an area from a file, or generate it from scratch.
+        
+        :return: the loaded or generated area.
+        :rtype: sw.area.Area
+        """
         # TODO: reading areas from a file
         import sw.area as area
         return area.area_from_scratch(self.data, self.biome, 20, 20)
@@ -35,7 +46,16 @@ class AreaHeader():
 
 
 def area_header_from_save(gamedata, yaml_dict):
-    """ Generate an area header from a YAML dict. """
+    """
+    Generate an area header from a YAML dict.
+
+    :param gamedata: a game data object.
+    :type gamedata: sw.gamedata.GameData
+    :param dict yaml_dict: a YAML dict with the saved header info.
+
+    :return: the generated header.
+    :rtype: AreaHeader
+    """
     raise NotImplementedError
 
 
@@ -43,7 +63,18 @@ def area_header_from_save(gamedata, yaml_dict):
 
 
 def area_header_from_scratch(gamedata, biome, arcanum_level):
-    """ Generate an area header with given parameters. """
+    """
+    Generate an area header with given parameters.
+
+    :param gamedata: a game data object.
+    :type gamedata: sw.gamedata.GameData
+    :param biome: the biome that should be used for the generated header.
+    :param ArcanumLevel arcanum_level: the level of arcanum corruption for the
+    generated header.
+
+    :return: the generated header.
+    :rtype: AreaHeader
+    """
     res = AreaHeader(gamedata)
     res.name = "TEMP AREA NAME"
     res.biome = biome
