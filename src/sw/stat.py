705f37014472ea0045ed0bb1be81c011d5b1797b
Michail Pevnev <mpevnev@gmail.com>
2018-04-29 17:17:15 +0300

    Update docstrings in the stat module

--- src/sw/stat.py
+++ src/sw/stat.py
@@ -25,42 +25,70 @@ class HasStats():
 
     @property
     def base_primary(self):
-        """ Return base primary statistics. """
+        """
+        :return: base primary statistics.
+        :rtype: dict
+        """
         return self.base_stats[stat.StatGroup.PRIMARY]
 
     @property
     def base_secondary(self):
-        """ Return base secondary statistics. """
+        """
+        :return: base secondary statistics.
+        :rtype: dict
+        """
         return self.base_stats[stat.StatGroup.SECONDARY]
 
     @property
     def total_primary(self):
-        """ Return total primary statistics. """
+        """
+        :return: total primary statistics.
+        :rtype: dict
+        """
         return self.total_stats[stat.StatGroup.PRIMARY]
 
     @property
     def total_secondary(self):
-        """ Return total secondary statistics. """
+        """
+        :return: total secondary statistics.
+        :rtype: dict
+        """
         return self.total_stats[stat.StatGroup.SECONDARY]
 
     #--------- statistics manipulation - setters ---------#
 
     @base_primary.setter
     def base_primary(self, new):
-        """" Set base primary statistics. """
+        """"
+        Set base primary statistics.
+
+        :param dict new: new dict with base primary statistics.
+        """
         self.base_stats[stat.StatGroup.PRIMARY] = new
 
     @base_secondary.setter
     def base_secondary(self, new):
-        """" Set base secondary statistics. """
+        """"
+        Set base secondary statistics.
+
+        :param dict new: new dict with base secondary statistics.
+        """
         self.base_stats[stat.StatGroup.SECONDARY] = new
 
     @total_primary.setter
     def total_primary(self, new):
-        """" Set total primary statistics. """
+        """"
+        Set total primary statistics.
+
+        :param dict new: new dict with total primary statistics.
+        """
         self.total_stats[stat.StatGroup.PRIMARY] = new
 
     @total_secondary.setter
     def total_secondary(self, new):
-        """" Set total secondary statistics. """
+        """"
+        Set total secondary statistics.
+
+        :param dict new: new dict with total secondary statistics.
+        """
         self.total_stats[stat.StatGroup.SECONDARY] = new
