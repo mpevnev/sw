5d69ea424b5a989e4b843e7f9e928b224da7f785
Michail Pevnev <mpevnev@gmail.com>
2018-04-29 13:02:39 +0300

    Update entity module docstrings

--- src/sw/entity.py
+++ src/sw/entity.py
@@ -8,7 +8,7 @@ Provides Entity class from which Doodad and Character classes inherit.
 class Entity():
     """
     A thing that can occupy a position and collide with other entities, on one
-    hand, and can die on the other.
+    hand, and that can die on the other.
     """
 
     def __init__(self):
@@ -18,29 +18,57 @@ class Entity():
     #--------- container logic ---------#
 
     def add_to_area(self, area):
-        """ Add this entity to the given area. """
+        """
+        Add this entity to a given area.
+
+        :param area: an area to add this entity to.
+        :type area: sw.area.Area
+        """
         raise NotImplementedError
 
     def remove_from_area(self, area):
-        """ Remove this entity from the given area. """
+        """
+        Remove this entity from a given area.
+        
+        :param area: an area from which the entity will be removed.
+        :type area: sw.area.Area
+        """
         raise NotImplementedError
 
     #--------- collision logic ---------#
 
     def add_collision_group(self, group):
-        """ Add a collision group to the entity. """
+        """
+        Add a collision group to the entity.
+        
+        :param group: a collision group to be added.
+        :type group: sw.const.entity.CollisionGroup
+        """
         self.collision_groups.add(group)
 
     def can_collide(self, other):
         """
-        Return True if the entity can collide with the other, False otherwise.
+        Test if the entity can collide with an other entity.
+
+        :param Entity other: the other entity.
+
+        :return: True if the entity can collide with the other, False
+        otherwise.
+        :rtype: bool
         """
         if self is other:
             return False
         return bool(self.collision_groups.intersection(other.collision_groups))
 
     def collides(self, other):
-        """ Return True if the entity collides with the other. """
+        """
+        Test if the entity collides with an other entity.
+
+        :param Entity other: the other entity.
+
+        :return: True if the entity collides with the other, False otherwise.
+        :rtype: bool
+        """
         if not self.can_collide(other):
             return False
         if self.position is None or other.position is None:
@@ -48,7 +76,15 @@ class Entity():
         return self.position == other.position
 
     def distance(self, other):
-        """ Return the Manhattan distance to the other entity. """
+        """
+        Calculate the Manhattan distance to an other entity.
+        
+        :param Entity other: an entity to which the distance will be
+        calculated.
+
+        :return: Manhattan distance to the other entity.
+        :rtype: int
+        """
         dx = self.position[0] - other.position[0]
         dy = self.position[1] - other.position[1]
         return max(abs(dx), abs(dy))
@@ -58,13 +94,24 @@ class Entity():
         self.position = None
 
     def hidden(self):
-        """ Return True if the entity is hidden, False otherwise. """
+        """
+        :return: True True if the entity is hidden, False otherwise.
+        :rtype: bool
+        """
         return self.position is None
 
     def would_collide(self, other, at_x, at_y):
         """
-        Return True if the entity, when placed at the position given by 'at_x'
-        and 'at_y', would collide with the other.
+        Test whether the entity would collide with another if placed at a given
+        position.
+
+        :param Entity other: the other entity.
+        :param int at_x: the X coordinate of the position to be tested.
+        :param int at_y: the Y coordinate of the position to be tested.
+
+        :return: True if the entity would collide with the other, False
+        otherwise.
+        :rtype: bool
         """
         old = self.position
         self.position = (at_x, at_y)
@@ -75,11 +122,23 @@ class Entity():
     #--------- death logic ---------#
 
     def alive(self):
-        """ Return True if the object is alive. """
+        """
+        :return: True if the entity is alive, False otherwise.
+        :rtype: bool
+        """
         raise NotImplementedError
 
     def death_action(self, state, area, ui):
-        """ This method will be called when this object dies. """
+        """
+        Do something when the entity dies.
+        
+        :param state: the global game environment of the entity.
+        :type state: sw.gamestate.GameState
+        :param area: the area containing the entity.
+        :type area: sw.area.Area
+        :param ui: the UI piece that should react to the death of the entity.
+        :type ui: sw.ui.MainDungeonWindow or None
+        """
         raise NotImplementedError
 
     def die(self):
@@ -90,20 +149,41 @@ class Entity():
 
     def transparent_for_player(self, player):
         """
-        Return True if the entity is transparent for the player, False
+        Test if the entity is transparent for a player.
+
+        :param player: a player being tested.
+        :type player: sw.player.Player
+
+        :return: True if the entity is transparent for the player, False
         otherwise.
+        :rtype: bool
         """
         raise NotImplementedError
 
     def transparent_for_monster(self, monster):
         """
-        Return True if the entity is transparent for the given monster, False
+        Test if the entity is transparent for a monster.
+
+        :param monster: a monster being tested.
+        :type monster: sw.monster.Monster
+
+        :return: True if the entity is transparent for the monster, False
         otherwise.
+        :rtype: bool
         """
         raise NotImplementedError
 
     #--------- other game logic ---------#
 
     def tick(self, state, area, ui):
-        """ Process a single game turn. """
+        """
+        Process a single game turn.
+        
+        :param state: global game environment of the entity.
+        :type state: sw.gamestate.GameState
+        :param area: the area containing the entity.
+        :type area: sw.area.Area
+        :param ui: the UI that should react to whatever happens in this turn.
+        :type ui: sw.ui.MainDungeonWindow
+        """
         raise NotImplementedError
