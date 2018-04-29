2763e966b0ed651b3577036dbf0f12c0c2df5b15
Michail Pevnev <mpevnev@gmail.com>
2018-04-29 13:19:36 +0300

    Update game state module docstrings

--- src/sw/gamestate.py
+++ src/sw/gamestate.py
@@ -21,15 +21,28 @@ class GameState():
         self.messages = None
 
     def current_overworld_header(self):
-        """ Return the header of the area where the player is located. """
+        """
+        :return: the header of the area where the player is located.
+        :rtype: sw.area_header.AreaHeader
+        """
         return self.world.area_headers[self.player_position]
 
 
 #--------- reading game state from a saved YAML dict ---------#
 
 
-def game_state_from_save(gamedata, yaml_dict):
-    """ Read game state from a saved YAML dict. """
+def game_state_from_save(gamedata, save):
+    """
+    Read game state from a save.
+
+    :param gamedata: an object with game data used to regenerate game objects.
+    :type gamedata: sw.gamedata.GameData
+    :param dict save: information about game objects, game configuration and so
+    on.
+
+    :return: regenerated game state object.
+    :rtype: GameState
+    """
     raise NotImplementedError
 
 
@@ -37,7 +50,19 @@ def game_state_from_save(gamedata, yaml_dict):
 
 
 def game_state_from_scratch(gamedata, player, world):
-    """ Create a GameState instance from separate parts. """
+    """
+    Create a GameState instance from separate parts.
+
+    :param gamedata: game data used to populate the state with game objects.
+    :type gamedata: sw.gamedata.GameData
+    :param player: previously created player object.
+    :type player: sw.player.Player
+    :param world: previously created world object.
+    :type world: sw.world.World
+
+    :return: new game state.
+    :rtype: GameState
+    """
     res = GameState()
     res.data = gamedata
     res.player = player
